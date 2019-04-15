45397fe76c8f830f7c629770f706d97e36da907e
raccony_ <vkrmk13@gmail.com>
2019-04-15 11:38:43 +0300

    Added phrases.php where all phrases is used by bot suppposed to keep

--- bot.php
+++ bot.php
@@ -4,7 +4,24 @@ require_once ("vendor/autoload.php");
 require_once ("secure.php");
 require_once ("modules/db_wrapper.php");
 require_once ("modules/json_wrapper.php");
+require_once ("phrases.php");
 
-//message processing mechanism will be here..
+// init
+$bot = new \TelegramBot\Api\Client($token);
+
+// message processing mechanism will be here..
+$bot->on(function($Update) use ($bot){
+    $phrases = $GLOBALS['phrases'];
+    $message = $Update->getMessage();
+    $text = $message->getText();
+    $chatid = $message->getChat()->getId();
+
+    $bot->sendMessage($chatid, $phrases['service_works']);
+
+}, function($message) use ($bot){
+    return true;
+});
+
+$bot->run();
 
 ?>
---
+++ phrases.php
@@ -0,0 +1,9 @@
+<?php
+
+//this file supposed to store all phrases used by bot
+
+$phrases = array(
+    'service_works' => "⚠ Ведуться технічні роботи"
+);
+
+?>
