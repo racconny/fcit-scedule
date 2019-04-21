60e2df62c2be49441f68a1a2a3f01ad50a20cbb5
raccony_ <vkrmk13@gmail.com>
2019-04-21 21:05:49 +0300

    Developed scenario till main menu, wrote some DB methods, updated phrases file

--- bot.php
+++ bot.php
@@ -10,9 +10,10 @@ require_once ("phrases.php");
 $bot = new \TelegramBot\Api\Client($token);
 $db = new DB();
 $schedule = 'resources/schedule.json';
+$json = new JSON($schedule);
 
 
-
+//listeners
 
 $bot->on(function($Update) use ($bot){
     if ($Update->getMessage()->getText()[0] !== "/"){
@@ -26,7 +27,17 @@ $bot->command('start', function ($message) use ($bot) {
     router($message, $bot);
 });
 
+$bot->command('wipe', function ($message) use ($bot) {
+    wipe($message, $bot);
+});
 
+//functions
+
+function wipe($message, $bot){
+    $db = $GLOBALS['db'];
+    $db->removeUser($message->getFrom()->getId());
+    $bot->sendMessage($message->getChat()->getId(), "Користувача видалено");
+}
 
 function router($message, $bot){
     $db = $GLOBALS["db"];
@@ -39,9 +50,21 @@ function router($message, $bot){
 
     //$bot->sendMessage($chatid, $phrases['service_works']);
 
-    if ($nav === -1){
-        $db->addUser($userid, $message->getFrom()->getFirstName(), $message->getFrom()->getLastName());
-        welcome($message, $bot);
+    switch ($nav) {
+       case -1:
+           welcome($message, $bot);
+           break;
+       case 0:
+           roleMenu($message, $bot);
+           break;
+       case 1:
+           setStudentCourse($message, $bot);
+           break;
+       case 11:
+           setStudentGroup($message, $bot);
+           break;
+       default:
+           $bot->sendMessage($chatid, "What the fuck It even supposed to mean?");
     }
 }
 
@@ -51,6 +74,9 @@ function welcome($message, $bot){
     $json = new JSON($GLOBALS['schedule']);
 
     //$groups = array_chunk($json->getCourses(), 3, false);
+
+    $db->addUser($message->getFrom()->getId(), $message->getFrom()->getFirstName(), $message->getFrom()->getLastName());
+
     $roles = array(array("Для студента"), array("Для викладача"), array("Для аудиторії"));
     $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup($roles, true);
 
@@ -58,6 +84,76 @@ function welcome($message, $bot){
 
     $bot->sendMessage($chatid, $phrases['welcome']);
     $bot->sendMessage($chatid, $phrases['select_role'], null, false, null, $keyboard);
+}
+
+function roleMenu($message, $bot){
+    $chatid = $message->getChat()->getId();
+    $phrases = $GLOBALS['phrases'];
+    $db = $GLOBALS['db'];
+    $json = $GLOBALS['json'];
+
+    if ($message->getText() === "Для студента"){
+        $db->updateUserNav($message->getFrom()->getId(), 1);
+        $courses = $json->getCourses();
+        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array_chunk($courses, 3, false), true);
+        $bot->sendMessage($chatid, $phrases['student_set_role']);
+        $bot->sendMessage($chatid, "Вибери свій курс: ", null, false, null, $keyboard);
+
+    } else if ($message->getText() === "Для викладача"){
+        $bot->sendMessage($chatid, $phrases['teacher_set_role']);
+        $db->updateUserNav($message->getFrom()->getId(), 2);
+    } else if ($message->getText() === "Для аудиторії"){
+        $bot->sendMessage($chatid, $phrases['audience_set_role']);
+        $db->updateUserNav($message->getFrom()->getId(), 3);
+    } else {
+        $bot->sendMessage($chatid, $phrases['invalid_input']);
+    }
+}
+
+function setStudentCourse($message, $bot){
+    $chatid = $message->getChat()->getId();
+    $text = $message->getText();
+
+    $phrases = $GLOBALS['phrases'];
+    $db = $GLOBALS['db'];
+    $json = $GLOBALS['json'];
+
+    $courses = $json->getCourses();
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array_chunk($courses, 3, false), true);
+
+    if (in_array($text, $courses)){
+        $bot->sendMessage($chatid, 'Курс встановлено: '.$text);
+        $db->updateUserNav($message->getFrom()->getId(), 11);
+        $db->updateUserCourse($message->getFrom()->getId(), $text);
+
+        $groups = $json->getGroupsForCourse($text);
+        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array_chunk($groups, 3, false), true);
+
+        $bot->sendMessage($chatid, $phrases['select_group'], null, false, null, $keyboard);
+    } else {
+        $bot->sendMessage($chatid, 'Неправильна назва курсу: '.$text.', спробуй ще раз', null, false, null, $keyboard);
+    }
+}
+
+function setStudentGroup($message, $bot){
+    $chatid = $message->getChat()->getId();
+    $text = $message->getText();
+
+    $phrases = $GLOBALS['phrases'];
+    $db = $GLOBALS['db'];
+    $json = $GLOBALS['json'];
+
+    $course = $db->getUserCourse($message->getFrom()->getId());
+    $groups = $json->getGroupsForCourse($course);
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(array_chunk($groups, 3, false), true);
+
+    if (in_array($text, $groups)){
+        $bot->sendMessage($chatid, 'Групу встановлено: '.$text);
+        $db->updateUserNav($message->getFrom()->getId(), 12);
+        $db->updateUserGroup($message->getFrom()->getId(), $text);
+    } else {
+        $bot->sendMessage($chatid, 'Неправильна назва групи: '.$text.', спробуй ще раз', null, false, null, $keyboard);
+    }
 }
 
 
--- modules/db_wrapper.php
+++ modules/db_wrapper.php
@@ -2,11 +2,13 @@
 
 require_once ("secure.php");
 
-class DB {
+class DB
+{
 
     private $connection;
 
-    public function __construct(){
+    public function __construct()
+    {
         $servername = "localhost";
         $username = $GLOBALS["db_username"];
         $password = $GLOBALS["db_pass"];
@@ -17,7 +19,8 @@ class DB {
     }
 
     //тестить чи ми законектились до БД
-    public function testConnection(){
+    public function testConnection()
+    {
         if ($this->connection->connect_error) {
             return $this->connection->connect_error;
         } else {
@@ -25,7 +28,8 @@ class DB {
         }
     }
 
-    public function getUserNavState($tgid){
+    public function getUserNavState($tgid)
+    {
         $sql = "select * from User where tg_id = $tgid";
         $result = $this->connection->query($sql);
 
@@ -37,11 +41,44 @@ class DB {
         }
     }
 
-    public function addUser($tgid, $name, $surname, $nav_state = 0){
+    public function getUserCourse($tgid){
+        $sql = "select * from User where tg_id = $tgid";
+        $result = $this->connection->query($sql);
+
+        if ($result->num_rows == 0) {
+            return -1;
+        } else {
+            $row = $result->fetch_assoc();
+            return $row['course'];
+        }
+    }
+
+    public function addUser($tgid, $name, $surname, $nav_state = 0)
+    {
         $sql = "insert into User (tg_id, name, surname, nav_state) values ($tgid, '$name', '$surname', $nav_state)";
         $result = $this->connection->query($sql);
     }
 
+    public function removeUser($tgid){
+        $sql = "delete from User where tg_id = $tgid";
+        $result = $this->connection->query($sql);
+    }
+
+    public function updateUserNav($tgid, $nav)
+    {
+        $sql = "update User set nav_state = '$nav' where tg_id = '$tgid'";
+        $result = $this->connection->query($sql);
+    }
+
+    public  function updateUserCourse($tgid, $course){
+        $sql = "update User set course = '$course' where tg_id = '$tgid'";
+        $result = $this->connection->query($sql);
+    }
+
+    public  function updateUserGroup($tgid, $group){
+        $sql = "update User set group_ = '$group' where tg_id = '$tgid'";
+        $result = $this->connection->query($sql);
+    }
 }
 
 ?>
